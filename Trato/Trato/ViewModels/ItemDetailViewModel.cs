ce23b33f2991c8533483179e4a1fc171c4d8b574
cesar augusto <augusto9623@hotmail.com>
2018-06-27 17:17:15 -0500

    dsadas

---
+++ Trato/Trato/ViewModels/ItemDetailViewModel.cs
@@ -0,0 +1,16 @@
+﻿using System;
+
+using Trato.Models;
+
+namespace Trato.ViewModels
+{
+    public class ItemDetailViewModel : BaseViewModel
+    {
+        public Item Item { get; set; }
+        public ItemDetailViewModel(Item item = null)
+        {
+            Title = item?.Text;
+            Item = item;
+        }
+    }
+}
