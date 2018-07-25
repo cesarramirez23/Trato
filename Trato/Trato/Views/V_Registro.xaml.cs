15ca4389d15382ccdb9e597650c79ad807915633
cesar augusto <augusto9623@hotmail.com>
2018-07-24 20:20:55 -0500

    correcion

--- Trato/Trato/Views/V_Registro.xaml.cs
+++ Trato/Trato/Views/V_Registro.xaml.cs
@@ -211,7 +211,7 @@ namespace Trato.Views
                     Uri url = new Uri("http://www.alsain.mx/trato_especial/tarjeta_alta.php");
 
                     HttpResponseMessage respuestaReg = await v_cliente.PostAsync(url, v_content);
-                    otroaa.Text = respuestaReg.ToString();
+                    otroaa.Text = v_content.ToString();
 
                     await DisplayAlert("Respuesta", respuestaReg.ToString(), "aceptar");
 
