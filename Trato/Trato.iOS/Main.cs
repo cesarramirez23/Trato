76cdaf3e9f2469d55e5575bed6d1811eb13676a2
cesarramirez23 <augusto9623@hotmail.com>
2020-06-16 10:43:47 -0500

    Limpia ios

--- Trato/Trato.iOS/Main.cs
+++ Trato/Trato.iOS/Main.cs
@@ -4,8 +4,6 @@ using System.Linq;
 
 using Foundation;
 using UIKit;
-using PayPal.Forms.Abstractions;
-using PayPal.Forms;
 
 namespace Trato.iOS
 {
@@ -17,24 +15,6 @@ namespace Trato.iOS
             // if you want to use a different Application Delegate class from "AppDelegate"
             // you can specify it here.
             UIApplication.Main(args, null, "AppDelegate");
-            var config = new PayPalConfiguration(PayPalEnvironment.Production, "AQrrFs8D-iSCYiAEEmO9ni3CcQ7GjgjPqSBBVhxNTmRKOnLR_Ol_qRcy2Pr4yxhwcQ2BK1BoZbzl0Hka")
-            {
-                //If you want to accept credit cards
-                AcceptCreditCards = false,
-                //Your business name
-                MerchantName = "Tienda",
-                //Your privacy policy Url
-                MerchantPrivacyPolicyUri = "http://tratoespecial.com/politicas-de-privacidad/",
-                //Your user agreement Url
-                MerchantUserAgreementUri = "http://tratoespecial.com/terminos-y-condiciones/",
-                // OPTIONAL - ShippingAddressOption (Both, None, PayPal, Provided)
-                ShippingAddressOption = ShippingAddressOption.Both,
-                // OPTIONAL - Language: Default languege for PayPal Plug-In
-                Language = "es",
-                // OPTIONAL - PhoneCountryCode: Default phone country code for PayPal Plug-In
-                PhoneCountryCode = "52",
-            };
-            CrossPayPalManager.Init(config);
         }
     }
 }
